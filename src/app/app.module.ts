54776a87a528aa7fa5d5fe40c8c2d31570e53a72
ravibk <ravibk8951@gmail.com>
2018-07-28 12:25:40 +0530

    Components part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 import { AppComponent } from './app.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 @NgModule({
@@ -17,6 +20,9 @@ import { MenuComponent } from './menu/menu.component';
     MatListModule,
    BrowserAnimationsModule,
    MatToolbarModule,
+   MatGridListModule,
+  MatCardModule,
+  MatButtonModule,
    FlexLayoutModule
   ],
   providers: [],
